6e379eb916ff0c3f96eaee83d76570a4f59f7e9d
swapnil pawar <swapnil.pawar9870@gmail.com>
2024-10-13 04:54:06 -0400

    Create a dealer details or reviews page

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,6 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Create your models here.
 
 # CarMake model to represent the make of a car
 class CarMake(models.Model):
@@ -19,17 +14,12 @@ class CarMake(models.Model):
             MaxValueValidator(now().year)  # Founding year can't be in the future
         ]
     )  
-    # website = models.URLField(
-    #     blank=True, null=True, 
-    #     validators=[URLValidator()]
-    # ) 
 
     def __str__(self):
         return f"{self.name} ({self.country})"
 
 # CarModel model to represent the model of a car
 class CarModel(models.Model):
-   
     CAR_TYPE_CHOICES = [
         ('Sedan', 'Sedan'),
         ('SUV', 'SUV'),
@@ -40,7 +30,6 @@ class CarModel(models.Model):
     dealer_id = models.IntegerField() 
     name = models.CharField(max_length=100) 
     type = models.CharField(max_length=10, choices=CAR_TYPE_CHOICES) 
-    year = models.DateField(default=now) 
     year = models.IntegerField(
         validators=[
             MinValueValidator(1886),  # Cars were invented in 1886, so minimum value is 1886
@@ -50,4 +39,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.type})"
-
